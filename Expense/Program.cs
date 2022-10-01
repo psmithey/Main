658527fcd9e7d1027a76a5b01cd70f690bc52218
tsmithe2 <thomas_smithey@yahoo.com>
2022-10-01 18:08:34 -0500

    Started writing program

---
+++ Expense/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+WriteLine("-----------------------------------------");
+WriteLine("|");
+WriteLine("| " + "Login(l) Register(r) Forgot Password(f) Exit(x) ");
