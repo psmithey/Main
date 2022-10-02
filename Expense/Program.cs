6b3ae3375e1ae72277b1efe1f26779ecd22728be
tsmithe2 <thomas_smithey@yahoo.com>
2022-10-01 19:01:56 -0500

    started writing code

--- Expense/Program.cs
+++ Expense/Program.cs
@@ -1,6 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 using System;
-WriteLine("-----------------------------------------");
-WriteLine("|");
-WriteLine("| " + "Login(l) Register(r) Forgot Password(f) Exit(x) ");
+bool exitApp = false;
+string ans;
+while (!exitApp)
+{
+    ans = Expenses.Login();
+    switch (ans)
+    {
+        case "x":
+            exitApp = true;
+            break;
+        default:
+            exitApp = true;
+            break;
+    }
+}
+public class Expenses
+{
+    public static string Login()
+    {
+        string ans;
+        string? userName;
+        string? passWord;
+        Console.WriteLine("");
+        Console.WriteLine("                   Expense Report");
+        Console.WriteLine(" -------------------------------------------------- ");
+        Console.WriteLine("| " + "Login(l) Register(r) Forgot Password(f) Exit(x) |");
+        Console.WriteLine(" -------------------------------------------------- ");
+        ans = Console.ReadLine();
+        switch (ans)
+        {
+            case "l":
+                Console.WriteLine("Enter Username");
+                userName = Console.ReadLine();
+                Console.WriteLine("Enter Password");
+                passWord = Console.ReadLine();
+                //check database for valid user
+                return "";
+                break;
+            case "r":
+
+                return "";
+                break;
+            case "f":
+
+                return "";
+                break;
+            case "x":
+                Console.WriteLine("Exit Program");
+                return "x";
+                break;
+            default:
+
+                return "z";
+                break;
+        }
+
+    }
+}
+    
