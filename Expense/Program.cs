257ea008819b1a8bbebe4eba15fd555344427790
tsmithe2 <thomas_smithey@yahoo.com>
2022-10-02 12:44:21 -0500

    setter and getter update

--- Expense/Program.cs
+++ Expense/Program.cs
@@ -32,7 +32,7 @@ while (!exitApp)
     }
     else
     { //there will be an if statement here once it's determined the user type to choose which class to call. admin, manager, or employee
-        validLogin = Expenses.Employee("Paul", "1");
+        validLogin = Expenses.Employee(response);
         if (validLogin == "r" || validLogin == "s" || validLogin == "p")
         {
             continue;
@@ -74,19 +74,25 @@ public class Login
                 Console.WriteLine("");
                 Console.WriteLine("Enter Username");
                 userName = Console.ReadLine();
-                exp.setUserName(userName);
+                //exp.setUserName(userName);
+
                 //check database for valid username
+
                 Console.WriteLine("");
                 Console.WriteLine("Enter Password");
                 passWord = Console.ReadLine();
-                exp.setPassWord(passWord);
+                //exp.setPassWord(passWord);
+
                 //check database for valid password
+
                 Console.WriteLine("");
                 Console.Clear();
                 Console.WriteLine("Credentials verified");
-                // Expenses.setFistName("Paul");
-                // Expenses.setUserId("1");
-                valid = "userId";
+                //exp.setFirstName("Paul"); //value will be from database querry
+                //exp.setUserId("1"); //value will be from database querry
+
+                valid = "Paul UserId"; //these values will be returned from a database querry
+
                 return valid; //need to return user ID pulled from database
                 break;
             case "r":
@@ -112,31 +118,35 @@ public class Login
 }
 public class Expenses
 {
-    private string username;
-    private string password;
+    private string firstName;
+    private string userId;
 
-    public string getUserName()
+    public string getFirstName()
     {
-        return this.username;
+        return this.firstName;
     }
-    public void setUserName(string value)
+    public void setFirstName(string value)
     {
-        this.username = value;
+        this.firstName = value;
+    }
+    public string getUserId()
+    {
+        return this.userId;
+    }
+    public void setUserId(string value)
+    {
+        this.userId = value;
     }
 
-    public string getPassWord()
+    public static string Employee(string response)
     {
-        return this.password;
-    }
-    public void setPassWord(string value)
-    {
-        this.password = value;
-    }
+        string[] words = response.Split(' ');
+        Expenses exp = new Expenses();
+        exp.setFirstName(words[0]);
+        exp.setUserId(words[1]);
 
-    public static string Employee(string firstName, string userId)
-    {
         string ans;
-        Console.WriteLine("Hello " + firstName + userId);
+        Console.WriteLine("User: " + words[0]);
         Console.WriteLine(" ----------------------------------------------------------------------------------------------------- ");
         Console.WriteLine("| " + "Submit new Expense Report(r) View all past submissions(s) View only pending submissions(p) Exit(x) |");
         Console.WriteLine(" ----------------------------------------------------------------------------------------------------- ");
